bf8d4e037d29fb25572a3473acf4402bfb2b60b7
JerryMissTom <582486638@qq.com>
2018-01-18 09:06:09 +0800

    添加了Tab隐藏和显示的功能

--- src/pages/tabs/tabs.ts
+++ src/pages/tabs/tabs.ts
@@ -1,4 +1,6 @@
-import { Component } from '@angular/core';
+import { Component, ElementRef, Renderer, ViewChild } from '@angular/core';
+import { Keyboard } from '@ionic-native/keyboard';
+import { Events, Tabs } from 'ionic-angular';
 
 import { AboutPage } from '../about/about';
 import { ContactPage } from '../contact/contact';
@@ -9,11 +11,45 @@ import { HomePage } from '../home/home';
 })
 export class TabsPage {
 
+  @ViewChild('myTabs') tabRef: Tabs;
+  mb: any;
+
   tab1Root = HomePage;
-  tab2Root = AboutPage;
-  tab3Root = ContactPage;
+  tab2Root = ContactPage;
+  tab3Root = AboutPage;
 
-  constructor() {
+  constructor(private elementRef: ElementRef,
+    private renderer: Renderer,
+    private keyboard: Keyboard,
+    private event: Events) {
+  }
 
+  ionViewDidLoad() {
+    let tabs = this.queryElement(this.elementRef.nativeElement, '.tabbar');
+    this.event.subscribe('hideTabs', () => {
+      console.log('hideTabs');
+      this.renderer.setElementStyle(tabs, 'display', 'none');
+      let SelectTab = this.tabRef.getSelected()._elementRef.nativeElement;
+      let content = this.queryElement(SelectTab, '.scroll-content');
+      this.mb = content.style['margin-bottom'];
+      this.renderer.setElementStyle(content, 'margin-bottom', '0')
+    });
+
+    this.event.subscribe('showTabs', () => {
+      console.log('showTabs');
+      this.renderer.setElementStyle(tabs, 'display', '');
+      let SelectTab = this.tabRef.getSelected()._elementRef.nativeElement;
+      let content = this.queryElement(SelectTab, '.scroll-content');
+      this.renderer.setElementStyle(content, 'margin-bottom', this.mb)
+    });
+  }
+
+  queryElement(elem: HTMLElement, q: string): HTMLElement {
+    return <HTMLElement>elem.querySelector(q);
+  }
+
+  ionViewWillUnload() {
+    this.event.unsubscribe('hideTabs');
+    this.event.unsubscribe('showTabs');
   }
 }
